4fb785925b1ba400054e810a7091407586079894
weison <weisonweileen@gmail.com>
2024-06-12 23:07:22 +0800

    ADD precise position control
    Signed-off-by: weison <weisonweileen@gmail.com>

--- example/main.cpp
+++ example/main.cpp
@@ -11,9 +11,11 @@ int main() {
     cmd.motorType = MotorType::GO_M8010_6;
     cmd.id = 0;
     cmd.mode = 1;
-    cmd.K_P = 0.00;
-    cmd.K_W = 0.1;
-    cmd.Pos = 0.0;
+    cmd.K_P = 1;
+    // cmd.K_P = 7;  这个值对应最大k_P
+    cmd.K_W = 0.03;
+    // 调3.14的coef就好对应0度位置
+    cmd.Pos = -5.1 - 3.14*2;
     // cmd.Pos = - 3.14 * 7 / 12 * 6.33;
     cmd.W = 0.0;
     cmd.T = 0.0;
@@ -29,11 +31,11 @@ int main() {
     //   std::cout <<  "motor.MError: " << data.MError <<  std::endl;
     //   std::cout <<  std::endl;
     // }
-    uint8_t *p = (uint8_t *)cmd.get_motor_send_data();
-    for (int i = 0; i < 17; i++)
+    // uint8_t *p = (uint8_t *)cmd.get_motor_send_data();
+    // for (int i = 0; i < 17; i++)
       // printf("0X%02X ", *p++);
 
-      data.get_motor_recv_data();
+    data.get_motor_recv_data();
     printf("position: %f\n", data.Pos);
 
     usleep(200);
